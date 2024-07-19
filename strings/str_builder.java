20ed52ec144b94f322987b0b29afbd42fa9904d6
sujeetyadav021 <yadavsujeet21@gmail.com>
2024-07-19 08:42:31 +0530

    added stringbuilder code

---
+++ strings/str_builder.java
@@ -0,0 +1,14 @@
+package strings;
+
+public class str_builder {
+    public static void main(String args[]){
+        StringBuilder sb = new StringBuilder("");
+
+        for(char ch='a'; ch<='z';ch++){
+            sb.append(ch);
+        }
+        System.out.println(sb);
+        System.out.println(sb.length());
+    }
+    
+}
